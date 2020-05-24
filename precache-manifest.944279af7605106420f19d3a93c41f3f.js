6a43e28b9ab80932194fd2e44bcac92b4faf9d03
Jayantkokitkar <kokitkarjayant@gmail.com>
2020-05-24 17:28:11 +0530

    Updates

---
+++ precache-manifest.944279af7605106420f19d3a93c41f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee110aada2a481c285e8b4d3429cd5d",
+    "url": "/justtrial/index.html"
+  },
+  {
+    "revision": "8f31634c1791605a671b",
+    "url": "/justtrial/static/css/main.4c7465db.chunk.css"
+  },
+  {
+    "revision": "486ca8e8b5cdbe992578",
+    "url": "/justtrial/static/js/2.1387e092.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/justtrial/static/js/2.1387e092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f31634c1791605a671b",
+    "url": "/justtrial/static/js/main.1e1f8ed4.chunk.js"
+  },
+  {
+    "revision": "ae95c28442da5d233ceb",
+    "url": "/justtrial/static/js/runtime-main.a306b53d.js"
+  }
+]);
